c74e54322ee741d87c0af2dbe66e31a4bac403a2
Tin test <dima20_2003@mail.ru>
2025-02-26 08:32:44 +0300

    Реализация пагинации и изменение недочётов

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,17 +1,28 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
-class ProductViewSet(ModelViewSet):
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title', 'description']
     search_fields = ['title', 'description']
 
-class StockViewSet(ModelViewSet):
+
+class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['products']
+    filterset_fields = ['products', 'address']
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -41,7 +41,19 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'logistic',
+
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
